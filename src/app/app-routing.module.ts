fdc7adf5c6cdd275bb38df3206dac0761aa2293e
Jeanne Siché <siche.jeanne@gmail.com>
2022-10-25 17:54:29 +0200

    initialisation routing et modules nécessaires

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LivresbygenreComponent } from './livresbygenre/livresbygenre.component';
 
 const routes: Routes = [
+  {path:'accueil', component:HomeComponent},
+  {path:'', redirectTo:'/accueil', pathMatch:'full'},
+  {path:'livres/genre', component:LivresbygenreComponent}
+
 ];
 
 @NgModule({
