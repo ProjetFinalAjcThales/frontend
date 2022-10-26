33d2f2ada925ce966c58273b4027a15154ff003e
Jeanne Siché <siche.jeanne@gmail.com>
2022-10-26 14:31:13 +0200

    creation component livre et implémentation ajout d'un livre à un panier

---
+++ src/app/livre/livre.component.ts
@@ -0,0 +1,70 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommandeLivre } from '../commande-livre';
+import { Livre } from '../livre';
+
+@Component({
+  selector: 'app-livre',
+  templateUrl: './livre.component.html',
+  styleUrls: ['./livre.component.css']
+})
+export class LivreComponent implements OnInit {
+  @Input() livre: Livre;
+
+  panierSession: Array<any>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.getPanierSession();
+  }
+
+
+  ajoutPanier(livrePanier: Livre) {
+    // je récupère mon panier en session
+    this.getPanierSession();
+    
+    // je crée une nouvelle ligne de commande (livre - quantité)
+    let ligneCommande = new CommandeLivre(livrePanier, 1);
+
+    // si mon panier en session est vide, je crée une 1ere ligne de commande
+    if(this.panierSession.length <= 0 ) {
+      this.panierSession.push(ligneCommande);
+     } else {
+
+      // renvoie true si le livre existe déjà dans le panier 
+      let isPresent = this.panierSession.some(function(el) { return el.livre.id == ligneCommande.livre.id});
+  
+      // si livre pas présent : je l'ajoute au panier
+      if (!isPresent) {
+        this.panierSession.push(ligneCommande);
+      } else {
+        // sinon j'augmente la quantité de 1
+        this.panierSession.forEach(element => {
+          if(element.livre.id === ligneCommande.livre .id) {
+            element.quantite = element.quantite + 1;
+          } 
+        })
+      }
+ 
+    }
+
+    // ajout du nouveau panier en session
+    sessionStorage.setItem("panier", JSON.stringify(this.panierSession));
+
+    // pour le test uniquement, je récupère le panier et je l'affiche en console pour vérification
+    this.getPanierSession();
+    console.log(this.panierSession);
+  }
+
+
+  // Si panier session null = initialisation array vide, sinon intinialisation à l'objet en session
+  getPanierSession() {
+    if(JSON.parse(sessionStorage.getItem("panier")) != null) {
+     this.panierSession = JSON.parse(sessionStorage.getItem("panier"));
+    } else {
+     this.panierSession= new Array<any>();
+    }
+  }
+
+}
