401d98778bbeb32e2ef9756d4feefd88984be6a1
Jeanne Siché <siche.jeanne@gmail.com>
2022-10-26 14:35:14 +0200

    modification lien panier pour afficher le panier

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommandeLivre } from '../commande-livre';
 import { GenreService } from '../genre.service';
 import { LivreService } from '../livre.service';
 import { LivresbygenreComponent } from '../livresbygenre/livresbygenre.component';
@@ -12,9 +13,27 @@ import { LivresbygenreComponent } from '../livresbygenre/livresbygenre.component
 export class HeaderComponent implements OnInit {
 
   listGenre: any;
+  panierSession: Array<CommandeLivre>;
+
   constructor(private genreService : GenreService,  private livreService: LivreService) { }
 
   ngOnInit(): void {
     this.genreService.getAllGenre().then(res => this.listGenre = res);
+
+
+    this.panierSession = JSON.parse(sessionStorage.getItem("panier"));
+    console.log(this.panierSession);
+  }
+
+  getInfoPanierSession() {
+    if(JSON.parse(sessionStorage.getItem("panier")) != null) {
+      this.panierSession = JSON.parse(sessionStorage.getItem("panier"));
+      this.panierSession.forEach(element => {
+        console.log("**** " +element.livre.titre + " quantité : " + element.quantite);
+      });
+     } else {
+      console.log("le panier est vide");
+     }
+
   }
 }
